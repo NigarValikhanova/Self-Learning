15dec2e9036ce8b62cbdca52fa2ba6cdbaf9a2c0
NigarValikhanova <imnigarvalikhanova@gmail.com>
2023-10-22 13:08:27 +0400

    First part

---
+++ Eleventh week in C#/Lesson21 02.10.2023/Lesson21 02.10.2023/Program.cs
@@ -0,0 +1,70 @@
+﻿using System.Diagnostics;
+using System.Runtime.InteropServices;
+
+namespace Lesson_21_02._10._2023
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            #region Multitasking
+            // islemeyen halda proqramdi, run oldusa prosese cevrilir
+            // CPU bir neche prosesi eyni anda isledirse bu multitaskingdi
+            #endregion
+
+            #region Multithreading
+            // bir proqramda bir neche metodu eyni anda isledirse multithreadingdir
+            // mesaj yaza yaza mail de gele biler for example
+            // shared memory -- proses ve threadin esas ferqi
+
+            //Thread thread = new Thread(TestX);
+            //thread.Start();
+            //TestY();
+
+            //Thread thread2 = new Thread(TestX);
+            //thread2.IsBackground = true; //main threadden asilidi. Main ishini bitiren kimi bitecek
+            //thread2.Start();
+            //TestY();
+            #endregion
+
+            #region ProsesClassi
+            //Process process = new Process();
+            //process.StartInfo.FileName = "calc.exe";
+            //process.Start();
+
+            //var pros = Process.GetCurrentProcess();
+            //Console.WriteLine(pros.ToString());// hal hazirda isleyen prosesi gosterir
+
+            //var pros1 = Process.GetProcesses();
+            //foreach (var pro in pros1)
+            //{
+            //    Console.WriteLine(pro.ProcessName);
+            //}
+            //Console.WriteLine("Axtaris et");
+            //string item = Console.ReadLine();
+            ////Console.WriteLine(item.Replace(" ", "%20"));
+            //WriteGoogle(item.Trim().Replace(" ", "%20"));
+
+            #endregion
+        }
+        public static void TestX()
+        {
+            for (int i = 0; i < 10000; i++)
+            {
+                Console.WriteLine(i + "XX");
+            }
+        }
+        public static void TestY()
+        {
+            for (int i = 0; i < 100; i++)
+            {
+                Console.WriteLine(i + "Y");
+            }
+        }
+        public static void WriteGoogle(string word) 
+        {
+            Process.Start("C:\\Program Files\\Google\\Chrome\\Application\\Chrome.exe", "https://www.google.com/search?q=" + word);
+        }
+    }
+}
---
+++ Eleventh week in C#/Lesson22 05.10.2023/Lesson22 05.10.2023/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Threading;
+using System.Security.Permissions;
+
+
+
+namespace Lesson_22_05._10._2023
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Race Condition
+            //for (int i = 0; i < 10; i++)
+            //{
+            //    new Thread(() => Console.WriteLine(i)).Start();
+            //}
+            //CPU anlamir ki, hansini isledib ya yox.
+            //mutleq shekilde hemin i-ni bir deyishene menimsetmek lazimdir
+
+            //for (int i = 0; i < 10; i++)
+            //{
+            //    int temp = i;
+            //    new Thread(() => Console.WriteLine(temp)).Start();
+            //}
+            #endregion
+
+            #region Abort, Join, Sleep, Priority
+            //Thread t1 = new Thread(TestX);
+            //t1.Start();
+            ////t1.Abort();
+
+            //Thread t2 = new Thread(TestY);
+            //t2.Start();
+
+            ////Join
+            //Thread t3 = new Thread(TestZ);
+            //t3.Start();
+            //t3.Join();
+            //Console.WriteLine("Work completed");
+
+            ////Sleep
+            //for (int i = 0; i < 10; i++)
+            //{
+            //   Console.WriteLine(i + " xxxxxx");
+            //    Thread.Sleep(2000);
+            //}
+
+            //// Priority
+            //Thread t4 = new Thread(TestZ);
+            //t4.Start();
+            //t4.Priority = ThreadPriority.Highest;
+
+            //for (int i = 0; i < 100; i++)
+            //{
+            //    Console.WriteLine("helloooo");
+            //}
+
+            #endregion
+
+        }
+        public static void TestX()
+        {
+            Console.WriteLine("Test X");
+        }
+        public static void TestY()
+        {
+            Console.WriteLine("Test Y");
+        }
+        public static void TestZ()
+        {
+            for (int i = 0; i < 100; i++)
+            {
+                Console.WriteLine("Work is in completed");
+            }
+        }
+    }
+}
