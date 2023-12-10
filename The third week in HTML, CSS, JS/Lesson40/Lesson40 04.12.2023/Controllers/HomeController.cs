c0460d694eddae6e26338eac762258230b546905
NigarValikhanova <imnigarvalikhanova@gmail.com>
2023-12-10 18:44:05 +0400

    Learning MVC in ASP.NET

---
+++ The third week in HTML, CSS, JS/Lesson40/Lesson40 04.12.2023/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using Lesson40_04._12._2023.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace Lesson40_04._12._2023.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            return View();
+        }
+
+        public IActionResult Services()
+        {
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            return View();
+        }
+
+    } 
+}
