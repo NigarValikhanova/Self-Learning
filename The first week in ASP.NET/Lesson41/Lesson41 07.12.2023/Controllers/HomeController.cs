45a2b24cc70291c40d571cfeaba24306314a7793
NigarValikhanova <imnigarvalikhanova@gmail.com>
2023-12-10 19:55:38 +0400

    Working cs and html file in the same page

---
+++ The first week in ASP.NET/Lesson41/Lesson41 07.12.2023/Controllers/HomeController.cs
@@ -0,0 +1,16 @@
+﻿using Lesson40_04._12._2023.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace Lesson40_04._12._2023.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+           List<ImagesItem> books = Cards.ImagesItems.ToList();
+            return View(books);
+        }
+
+    } 
+}
