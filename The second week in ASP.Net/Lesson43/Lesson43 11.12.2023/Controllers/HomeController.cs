ba71b11c276cefcefd57710999ca6927b69b571f
NigarValikhanova <imnigarvalikhanova@gmail.com>
2023-12-12 22:27:41 +0400

    Code first connection SQL

---
+++ The second week in ASP.Net/Lesson43/Lesson43 11.12.2023/Controllers/HomeController.cs
@@ -0,0 +1,40 @@
+﻿using Lesson40_04._12._2023.DAL;
+using Lesson40_04._12._2023.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Diagnostics;
+
+namespace Lesson40_04._12._2023.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly AppDbContext _appContext;
+        //public readonly DbConfig _dbConfig;
+        //public readonly IServiceProvider _serviceProvider;
+        //public HomeController(AppDbContext appContext, DbConfig dbConfig, IServiceProvider serviceProvider)
+        //{
+        //    _dbConfig = dbConfig;
+        //    _serviceProvider = serviceProvider;
+        //    _appContext = appContext;
+        //}
+
+        public HomeController(AppDbContext appContext)
+        {
+            _appContext = appContext;
+        }
+
+        public IActionResult Index()
+        {
+            //List<ImagesItem> books = Cards.ImagesItems.ToList();
+            //return View(books);
+            return View();
+        }
+
+        public async Task< IActionResult> Contact()
+        {
+            List<Product> products = await _appContext.Products.Include(x=>x.Category).Where(z=>z.Category.CategoryName=="Electronics").ToListAsync();
+            return View(products);
+        }
+
+    } 
+}
---
+++ The second week in ASP.Net/Lesson43/Lesson43 11.12.2023/Migrations/20231212174100_addImageUrlToProducts.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Lesson4004._12._2023.Migrations
+{
+    /// <inheritdoc />
+    public partial class addImageUrlToProducts : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "ImageUrl",
+                table: "Products",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ImageUrl",
+                table: "Products");
+        }
+    }
+}
